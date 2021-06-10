aec0a8dab02bd6d48b790684b63d75d02b6be9a5
TomuraCreator <vladislavutkin92@gmail.com>
2021-06-10 17:13:58 +0300

    initial commit

---
+++ src/main/java/ru/tcreator/Randoms.java
@@ -0,0 +1,31 @@
+package ru.tcreator;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int max;
+    protected int min;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(max - min) + min;
+            }
+        };
+    }
+}
